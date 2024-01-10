22640e1a732f08168f7eff5410f84405bbe2d7f5
QuangThanhG13 <129851317+thanh-leo10@users.noreply.github.com>
2024-01-10 17:26:04 +0700

    Day2

---
+++ Thanh/Day2/For_While_DoWhile.java
@@ -0,0 +1,26 @@
+public class For_While_DoWhile {
+    public static void main(String[] args) {
+        int sum = 0;
+        int sum1 = 0;
+        int sum3 = 0;
+
+
+        for (int i = 1; i <= 1000; i++) {
+            sum += i;
+        }
+        int a =1;
+        while (a <= 1000){
+            sum1 += a;
+            a++ ;
+        }
+       int b = 1;
+        do {
+            sum3 += b;
+            b++;
+        }while (b <= 1000);
+
+        System.out.println("Sum use for : " + sum);
+        System.out.println("Sum use While : " + sum1);
+        System.out.println("Sum sue do-While : " + sum3);
+    }
+}
