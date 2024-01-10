2525a5c76a23f5268c45c0495a1094966aa11f18
NgocDungK68 <dungtranngoc1412@gmail.com>
2024-01-10 19:57:01 +0700

    Day2

---
+++ Dung/DAY2/ex1_1.java
@@ -0,0 +1,13 @@
+//tong cac so nguyen tu 1 den 1000: for
+public class ex1_1 {
+    public static void main(String[] args) {
+
+        int i, sum = 0;
+        for ( i = 1; i <= 1000 ; i++ )
+        {
+            sum += i;
+        }
+
+        System.out.println("Tong cac so nguyen tu 1 den 1000 la: " + sum);
+    }
+}
