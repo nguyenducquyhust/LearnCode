396e868648baed56a0d37e12478075c166996606
nguyenducquyhust <nguyenducquy.hust@gmail.com>
2024-01-10 20:50:57 +0700

    format folder

--- KyKhai/DAY1/bai3.java
+++
@@ -1,7 +0,0 @@
-public class bai3 {
-    public static void main(String[] args) {
-        String cau = "Ah Hoa! Ah Dao. Hien ah, Thao My";
-        String replacestring = cau.replaceAll("ah","Anh");
-        System.out.println(replacestring);
-    }
-}
