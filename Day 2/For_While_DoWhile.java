1c43813d2980fb77af6e58c6de850ce9784616d4
QuangThanhG13 <129851317+thanh-leo10@users.noreply.github.com>
2024-01-10 22:53:04 +0700

    Update For_While_DoWhile.java

--- Day 2/For_While_DoWhile.java
+++ Day 2/For_While_DoWhile.java
@@ -1,26 +1,39 @@
 public class For_While_DoWhile {
     public static void main(String[] args) {
-        int sum = 0;
-        int sum1 = 0;
-        int sum3 = 0;
+//        int sum = 0;
+//        int sum1 = 0;
+//        int sum3 = 0;
+//
+//
+//        for (int i = 1; i <= 1000; i++) {
+//            sum += i;
+//        }
+//
+//        int a =1;
+//        while (a <= 1000){
+//            sum1 += a;
+//            a++ ;
+//        }
+//
+//        int b = 1;
+//        do {
+//            sum3 += b;
+//            b++;
+//        } while (b <= 1000);
+//
+//        System.out.println("Sum use for : " + sum);
+//        System.out.println("Sum use While : " + sum1);
+//        System.out.println("Sum sue do-While : " + sum3);
 
-
-        for (int i = 1; i <= 1000; i++) {
-            sum += i;
+        for (int c = 1; c <= 10; c++) {
+            System.out.println(c);
         }
-        int a =1;
-        while (a <= 1000){
-            sum1 += a;
-            a++ ;
-        }
-       int b = 1;
-        do {
-            sum3 += b;
-            b++;
-        }while (b <= 1000);
 
-        System.out.println("Sum use for : " + sum);
-        System.out.println("Sum use While : " + sum1);
-        System.out.println("Sum sue do-While : " + sum3);
+        int d = 0;
+        while (d <= 10 ){
+            d++;
+            System.out.println(d);
+        }
+
     }
 }
