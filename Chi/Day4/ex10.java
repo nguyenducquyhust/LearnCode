850baeba68f10c89a2dca2ce273fef6f670270ce
quynhchi03 <buithichi1998hb@gmail.com>
2024-01-21 18:25:05 +0700

    Day4

---
+++ Chi/Day4/ex10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ex10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        System.out.println("Nhap so n phan tu cua mang : ");
+        n = scanner.nextInt();
+        if (n < 0) {
+            System.out.println("Gia tri cua n khong hop le. Vui long nhap lai n.");
+            return;
+        }
+        int[] a = new int[1000];
+        System.out.println(" n phan tu bao gom : ");
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int Max = 0;
+        for (int j = 1; j <= n; j++) {
+            if ((a[j] > Max) && (a[j] > 0)) {
+                Max = a[j];
+            }
+        }
+        System.out.println("Gia tri duong lon nhat trong mang la: " + Max);
+    }
+}
